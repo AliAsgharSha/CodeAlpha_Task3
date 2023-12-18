db550d1c737c48bcc5848e461da7c1f55d323cc2
Ali Asghar Shah <147384233+AliAsgharSha@users.noreply.github.com>
2023-12-19 01:39:40 +0500

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -1,7 +1,6 @@
 import nltk
 from nltk.chat.util import Chat, reflections
 
-# Define pairs of patterns and responses for the chatbot
 pairs = [
     ['my name is (.*)', ['Hello %1, how can I help you today?']],
     ['(hi|hello|hey|hola)', ['Hi there!', 'Hello!', 'Hey!']],
@@ -13,12 +12,10 @@ pairs = [
     ['exit', ['Goodbye!', 'Bye! Take care.']]
 ]
 
-# Create a chatbot
 def basic_chatbot():
     print("Welcome to Chatbot! Type 'exit' to end the conversation.")
     print("Hello, I'm Bot How can I help you")
     chatbot = Chat(pairs, reflections)
     chatbot.converse()
 
-# Run the chatbot
 basic_chatbot()
